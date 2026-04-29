eeacf4f6b0f78e23ea8fd484a5ef877abfbb9019
Dareck72 <dossouaurel72@gmail.com>
2026-04-29 16:42:09 +0100

    uploading image and name of disease

--- lib/App/modules/models/dataModel.dart
+++ lib/App/modules/models/dataModel.dart
@@ -1,32 +1,28 @@
 import 'package:image_picker/image_picker.dart';
 
 class DataModel {
-  final XFile image;
-  final String name;
-  final String probability;
-  final DateTime time;
+  final XFile image_path;
+  final String disease_name;
+  final DateTime created_at;
 
   DataModel({
-    required this.image,
-    required this.name,
-    required this.probability,
-    required this.time,
+    required this.image_path,
+    required this.disease_name,
+    required this.created_at    ,
   });
 
   factory DataModel.fromjson(Map data) {
     return DataModel(
-      image: data["image"],
-      name: data["name"],
-      probability: data["probability"],
-      time: data["time"],
+      image_path: data["file"],
+      disease_name: data["disease_name"],
+      created_at: data["created_at"],
     );
   }
   toMap(DataModel map) {
     return{
-      "image":map.image,
-      "name":map.name,
-      "probability":map.probability,
-      "time":map.time,
+      "image_path":map.image_path,
+      "disease_name":map.disease_name,
+      "created_at":map.created_at,
     };
   }
 }
